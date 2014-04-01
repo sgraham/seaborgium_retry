c5f28c2759392cd6a7e3076a54c6af988245eec7
Scott Graham <scottmg@chromium.org>
2014-03-31 20:47:12 -0700

    sdl wip

--- src/sg_exe/main.cc
+++ src/sg_exe/main.cc
@@ -2,9 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stdio.h>
 #include "SDL/include/SDL.h"
 
 int main() {
-  SDL_Init(SDL_INIT_VIDEO);
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Window* win = SDL_CreateWindow(
+      "Seaborgium", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+  if (!win) {
+    fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Quit();
   return 0;
 }
